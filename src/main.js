1f1e3a120c14565e60f4c8a36841426429477d08
PriscillaTrevizan <105805665+PriscillaTrevizan@users.noreply.github.com>
2023-01-21 23:47:51 -0300

    css commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { nanoid } from "nanoid"
+import './style.css'
+
+const passwordBtnEl =  document.querySelector('button')
+const diplayPasswordEl = document.querySelector('h2')
+
+passwordBtnEl.addEventListener('click', () => {
+     const randomPassword = nanoid()
+     diplayPasswordEl.innerHTML = randomPassword
+})
